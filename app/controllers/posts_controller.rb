d8d6e70b506695914a1ba53831bab212f5d91904
Rob Sturcke <rs@rsvision.com>
2016-01-07 00:25:42 -0500

    devise added alomng with controllers etc

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  belongs_to :user
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:notice] = "Posted!"
+      redirect_to @post
+    else
+      flash[:error] = "Not posted, try again!"
+      render :new
+    end
+  end
+
+  def update
+    @post.assign_attributes(post_params)
+    if @post.save
+      flash[:notice] = "Updated!"
+      redirect_to @post
+    else
+      flash[:error] = "Not updated, try again!"
+      render :new
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      flash[:notice] = "Deleted your posting!"
+      redirect_to posts_path
+    else
+      flash[:error] = "Posting not killed, try again!"
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name)
+  end
+end
